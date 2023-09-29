1dd5fce82e23d9bf0301c7cdcec5168a892ba678
Ismaelleon <Ismaelleon@protonmail.com>
2023-09-29 01:34:40 -0300

    feat: add delete pin button

--- src/client/src/routes/pin.tsx
+++ src/client/src/routes/pin.tsx
@@ -1,46 +1,77 @@
 import { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
 import { Link, useLocation } from "react-router-dom";
-import { BiSolidUserCircle } from "react-icons/bi";
+import { BiDotsHorizontal, BiDownload, BiSolidUserCircle, BiTrash } from "react-icons/bi";
+import { useQuery } from "react-query";
 
 export default function Pin() {
-	const [pin, setPin] = useState({
-		image: {
-			url: "",
-			public_id: "",
-		},
-		title: "",
-		content: "",
-		author: "",
-	});
+    const [userData, setUserData] = useState({
+        name: '',
+    });
+    const [options, setOptions] = useState(false);
 	const location = useLocation();
 
-	function getPin() {
-		fetch(`/api/pin/${location.pathname.split("/")[2]}`, {
-			method: "POST",
-		}).then(async (res) => {
-			if (res.status === 200) {
-				let pin = await res.json();
+    const { isLoading, error, data } = useQuery('userData', async () => {
+        try {
+            const res = await fetch(`/api/pin/${location.pathname.split("/")[2]}`, {
+                method: "POST",
+            });			
 
-				setPin(pin);
-			}
-		});
-	}
+            if (res.status === 200) {
+                getUserData();
+                return res.json();
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    });
 
-	useEffect(getPin, []);
+    async function getUserData () {
+        const res = await fetch('/api/user', {
+            method: 'POST',
+        });
+
+        if (res.status === 200) {
+            let user = await res.json();
+            setUserData(user);
+        }
+    }
+
+    if (isLoading) return 'Loading...';
+
+    if (error) return 'Error';
 
 	return (
 		<>
 			<Navbar />
 			<main className="flex justify-center mt-[64px] p-4">
 				<section className="flex flex-col max-w-3xl w-full sm:flex-row gap-3">
-					<img src={pin.image.url} className="w-full rounded mb-3 sm:w-1/2" />
+					<img src={data.image.url} className="w-full rounded mb-3 sm:w-1/2" />
 					<section className="sm:w-1/2">
-						<h2 className="text-xl font-bold mb-2">{pin.title}</h2>
-						<p className="text-base mb-3">{pin.content}</p>
-                        <Link to={`/user/${pin.author}`}  className="text-base font-bold flex flex-row items-center gap-3 mb-2">
-                            <BiSolidUserCircle size={24} /> {pin.author}
-                        </Link>
+                        <header className="py-2">
+                            <button className={`p-2 hover:bg-neutral-200 rounded-full ${options ? 'bg-neutral-200' : ''}`}
+                                    onClick={() => setOptions(!options)}>
+                                <BiDotsHorizontal size={24} />
+                                <ul className="flex-col list-none absolute bg-white shadow p-2 rounded-sm mt-3 -ml-2"
+                                    style={options ? { display: 'flex' } : { display: 'none' }}>
+                                    <li className="flex flex-row items-center text-left p-2 hover:bg-neutral-200 rounded-sm" onClick={() => window.open(data.image.url)}>
+                                        <BiDownload className="float-left ml-1 mr-2" size={18} /> Download Image
+                                    </li>
+                                    {userData.name === data.author && (
+                                        <li className="flex flex-row items-center text-left p-2 hover:bg-neutral-200 rounded-sm text-red-500">
+                                            <BiTrash className="float-left ml-1 mr-2" size={18} />Delete Pin
+                                        </li>
+                                    )}
+                                </ul>
+                            </button>
+                        </header>
+                        <section className="p-2">
+                            <h2 className="text-xl font-bold mb-2">{data.title}</h2>
+                            <p className="text-base mb-3">{data.content}</p>
+                            <Link to={`/user/${data.author}`}  className="text-base font-bold flex flex-row items-center gap-3 mb-2">
+                                <BiSolidUserCircle size={24} /> {data.author}
+                            </Link>
+                        </section>
 					</section>
 				</section>
 			</main>
