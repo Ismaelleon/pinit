e0c254ee2eaeafd3a947aa22f44fe1c22c58621f
Ismaelleon <Ismaelleon@protonmail.com>
2023-10-23 05:02:38 -0300

    fix: use useRef

--- src/client/src/routes/create.tsx
+++ src/client/src/routes/create.tsx
@@ -4,18 +4,16 @@ import Navbar from "../components/navbar";
 import { BiPlus } from "react-icons/bi";
 
 export default function Create() {
-	const [title, setTitle] = useState(""),
+	const [title, setTitle] = useState({ value: '', error: false }),
 		[content, setContent] = useState(""),
 		[url, setUrl] = useState(""),
-		[image, setImage] = useState<any>(null),
+		[image, setImage] = useState<any>({ value: null, error: false }),
 		[board, setBoard] = useState("new-board"),
 		[boards, setBoards] = useState([]),
-		[newBoard, setNewBoard] = useState("");
-
-	const select: LegacyRef<HTMLSelectElement> = createRef();
+		[newBoard, setNewBoard] = useState({ value: '', error: false });
 
 	const navigate = useNavigate();
-  const newBoardInput: LegacyRef<HTMLInputElement> = useRef(null);
+    const newBoardInput: LegacyRef<HTMLInputElement> = useRef(null);
 
 	function getUser() {
 		fetch("/api/user/", {
@@ -37,7 +35,7 @@ export default function Create() {
 		fetch("/api/board/new", {
 			method: "POST",
 			body: JSON.stringify({
-				name: newBoard,
+				name: newBoard.value,
 			}),
 			headers: {
 				"Content-Type": "application/json",
@@ -61,10 +59,10 @@ export default function Create() {
 
 		const body = new FormData();
 
-		body.append("title", title);
+		body.append("title", title.value);
 		body.append("content", content);
 		body.append("url", url);
-		body.append("image", image);
+		body.append("image", image.value);
 		body.append("boardName", board);
 
 		fetch("/api/pin/new", {
@@ -74,7 +72,15 @@ export default function Create() {
 			if (res.status === 200) {
 				let { id } = await res.json();
 				navigate(`/pin/${id}`);
-			}
+			} else {
+                if (title.value.length < 4) {
+                    setTitle({ value: title.value, error: true });
+                }
+
+                if (image.value === null) {
+                    setImage({ value: null, error: true });
+                }
+            }
 		});
 	}
 
@@ -96,14 +102,14 @@ export default function Create() {
 								multiple={false}
 								onInput={(e) =>
                                     // @ts-ignore
-									setImage(e.currentTarget.files[0])
+									setImage({ value: e.currentTarget.files[0], error: false })
 								}
 							/>
-                            <img className="w-full rounded" src={image === null ? '' : URL.createObjectURL(image)}/>
-                            <header className="flex flex-row justify-end p-2" style={ image === null ? { display: 'none' } : { display: 'flex' } }>
+                            <img className="w-full rounded" src={image.value === null ? '' : URL.createObjectURL(image.value)}/>
+                            <header className="flex flex-row justify-end p-2" style={ image.value === null ? { display: 'none' } : { display: 'flex' } }>
                                 <button className="p-2 rounded-full bg-neutral-200" onClick={e => {
                                     e.preventDefault();
-                                    setImage(null);
+                                    setImage({ value: null, error: false });
                                 }}>
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
@@ -120,28 +126,29 @@ export default function Create() {
                                 </button>
                             </header>
 							<label
-								className="w-full p-3 flex justify-center items-center flex-col border border-dashed border-neutral-400 rounded mb-3 cursor-pointer h-full"
+								className={`w-full p-3 flex justify-center items-center flex-col border border-dashed rounded mb-3 cursor-pointer h-full ${image.error ? 'border-red-400' : 'border-neutral-400'}`}
 								htmlFor="image"
-                                style={ image === null ? { display: 'flex' } : { display: 'none' } }
+                                style={ image.value === null ? { display: 'flex' } : { display: 'none' } }
 							>
 								<BiPlus size={32} />
 								<span className="text-base sm:text-sm">
 									Insert Image
 								</span>
 							</label>
+                            <span className={`text-sm mt-1 block mr-auto text-red-400 ${image.error ? 'block' : 'hidden'}`}>We need an image to upload</span>
 						</section>
 						<section className="sm:w-1/2">
 							<input
 								type="text"
 								placeholder="Add a Title"
-								className="border-neutral-400 border text-base rounded w-full p-2 mb-3 sm:text-sm"
-								onInput={(e) => setTitle(e.currentTarget.value)}
+								className={`border text-base rounded w-full p-2 sm:text-sm ${title.error ? 'border-red-400' : 'border-neutral-400'}`}
+								onInput={(e) => setTitle({ value: e.currentTarget.value, error: false })}
 							/>
                             <span className={`text-sm mt-1 block mr-auto text-red-400 ${title.error ? 'block' : 'hidden'}`}>Pin title is too short (minimum 4 characters)</span>
 							<textarea
 								placeholder="Tell everyone about your Pin."
 								rows={4}
-								className="border-neutral-400 border text-base rounded w-full p-2 mb-3 resize-none sm:text-sm"
+								className="border-neutral-400 border text-base rounded w-full p-2 mt-3 mb-3 resize-none sm:text-sm"
 								onInput={(e) =>
 									setContent(e.currentTarget.value)
 								}
@@ -155,10 +162,10 @@ export default function Create() {
 							<select
 								className="border-neutral-400 border text-base rounded w-full p-2 mb-3 bg-white sm:text-sm"
 								onChange={(e) => {
-									setBoard(e.currentTarget.value);
-                  setNewBoard({ value: newBoard.value, error: false });
+									setBoard(e.currentTarget.value)
+                                    setNewBoard({ value: newBoard.value, error: false });
 								}}
-                value={board}
+                                value={board}
 							>
 								<option value="new-board">
 									Create new Board
@@ -177,17 +184,17 @@ export default function Create() {
 										: { display: "none" }
 								}
 							>
-                <div className="flex flex-col">
-                    <input
-                        type="text"
-                        placeholder="Board name"
-                        className={`border text-base rounded w-75 p-2 bg-white sm:text-sm ${newBoard.error ? 'border-red-400' : 'border-neutral-400'}`}
-                        onInput={(e) =>
-                            setNewBoard({ value: e.currentTarget.value, error: false })
-                        }
-                        ref={newBoardInput}
-                    />
-                </div>
+                                <div className="flex flex-col">
+                                    <input
+                                        type="text"
+                                        placeholder="Board name"
+                                        className={`border text-base rounded w-75 p-2 bg-white sm:text-sm ${newBoard.error ? 'border-red-400' : 'border-neutral-400'}`}
+                                        onInput={(e) =>
+                                            setNewBoard({ value: e.currentTarget.value, error: false })
+                                        }
+                                        ref={newBoardInput}
+                                    />
+                                </div>
 								<button
 									className="bg-red-600 hover:bg-red-800 w-full text-base text-white font-semibold rounded sm:text-sm self-start p-2 border border-red-600 hover:border-red-800"
 									onClick={createBoard}
