67f90ff7cab8e3bc5115c9d0ebdde56f78b7f961
Ismaelleon <Ismaelleon@protonmail.com>
2024-03-21 16:38:15 -0300

    feat: add dropdown to navbar

--- src/client/src/components/navbar.tsx
+++ src/client/src/components/navbar.tsx
@@ -1,8 +1,11 @@
-import { Link, useLocation } from 'react-router-dom';
-import { BiHome, BiPlus, BiSolidUserCircle } from 'react-icons/bi';
+import React, { useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { BiChevronDown, BiHome, BiPlus, BiSolidUserCircle, BiSolidCog, BiExit } from 'react-icons/bi';
 
 export default function Navbar() {
     const location = useLocation();
+    const navigate = useNavigate();
+	const [accountDropDown, setAccountDropDown] = useState(false);
 
     return (
         <nav className="flex w-full justify-center py-2 px-4 fixed left-0 top-0 bg-white items-center z-50">
@@ -41,10 +44,42 @@ export default function Navbar() {
                             <p className="hidden sm:flex">Create</p>
                         </Link>
                     </li>
-                    <li>
-                        <Link to="/profile">
-                            <BiSolidUserCircle size={32} />
-                        </Link>
+                    <li className="flex flex-col">
+						<span 
+							className="flex flex-row items-center relative cursor-pointer"
+							onClick={() => setAccountDropDown(!accountDropDown)}
+						>
+							<BiSolidUserCircle size={32} />
+							<BiChevronDown size={16} />
+						</span>
+						<ul className={`fixed top-[40px] bg-white p-2 shadow-lg -translate-x-full ml-[48px] ${!accountDropDown && 'hidden'}`}>
+							<li className="pr-2">
+								<Link to="/profile" className="flex flex-row items-center gap-2 hover:underline">
+									<BiSolidUserCircle size={16} />
+									Profile
+								</Link>
+							</li>
+							<li className="pr-2">
+								<Link to="/settings" className="flex flex-row items-center gap-2 hover:underline">
+									<BiSolidCog size={16} />
+									Settings	
+								</Link>
+							</li>
+							<li className="pr-2">
+								<Link 
+									to="#" 
+									className="flex flex-row items-center gap-2 hover:underline"
+                                    onClick={() => {
+                                        document.cookie =
+                                            'token=;Expires=Thu, 01 Jan 1970 00:00:00 UTC;';
+                                        navigate('/');
+                                    }}
+								>
+									<BiExit size={16} className="text-red-600" />
+									Log Out	
+								</Link>
+							</li>
+						</ul>
                     </li>
                 </ul>
             </div>
