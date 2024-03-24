b7d7200a741c750596f7f6182b98511b0a9046b7
Ismaelleon <Ismaelleon@protonmail.com>
2024-03-24 20:48:29 -0300

    feat: use localstorage to save theme preferences

--- src/client/src/context/dark-mode.tsx
+++ src/client/src/context/dark-mode.tsx
@@ -1,4 +1,4 @@
-import { Context, ReactNode, createContext, useState } from 'react';
+import { Context, ReactNode, createContext, useEffect, useState } from 'react';
 
 interface Props {
 	children: ReactNode;
@@ -7,12 +7,21 @@ interface Props {
 export const DarkModeContext: Context<object> = createContext({});
 
 export default function DarkModeProvider (props: Props) {
-	const [darkMode, setDarkMode] = useState(false);
+	const darkModeValue = localStorage.getItem('dark-mode') === 'true';
+
+	const [darkMode, setDarkMode] = useState(darkModeValue || false);
 	
 	function toggleDarkMode (): void {
-		setDarkMode(!darkMode);
 		document.documentElement.classList.toggle('dark');
+		localStorage.setItem('dark-mode', String(!darkMode));
+		setDarkMode(!darkMode);
 	}
+
+	useEffect(() => {
+		if (darkMode) {
+			document.documentElement.classList.add('dark');
+		}
+	}, [])
 
 	return (
 		<>
