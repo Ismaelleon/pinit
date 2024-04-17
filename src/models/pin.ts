107af98b14a92dc6f4e1534a74b37950de4112bf
Ismaelleon <Ismaelleon@protonmail.com>
2024-04-17 20:09:38 -0300

    fix: set the replies type to an array of objects

--- src/models/pin.ts
+++ src/models/pin.ts
@@ -21,6 +21,18 @@ const pinSchema = new mongoose.Schema({
                 author: String,
                 date: String,
                 likes: [String],
+				replies: {
+					type: [
+						{
+							content: String,
+							author: String,
+							date: String,
+							likes: [String],
+							_id: mongoose.Schema.Types.ObjectId,
+						},
+					],
+					default: [],
+				},
                 _id: mongoose.Schema.Types.ObjectId,
             },
         ],
